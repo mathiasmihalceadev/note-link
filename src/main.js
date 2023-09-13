6c2242dc42b39479780587dc1d8ba88a7c8cbe79
mathiasmihalceadev <mathias.mihalcea@gmail.com>
2023-09-13 14:48:59 +0300

    Initial commit for Github Pages branch

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
-import router from './router'
-import 'animate.css'
-import {createPinia} from 'pinia'
-
-createApp(App).use(createPinia()).use(router).mount('#app')
-
